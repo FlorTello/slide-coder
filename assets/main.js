6da79ff8d4833c88fe3eaa19a955a8a3fa5630b1
Flor Tello <flor.26071994@gmail.com>
2017-04-04 13:32:50 -0500

    Agregando validaciones

--- assets/main.js
+++ assets/main.js
@@ -2,12 +2,18 @@ var dfm = document.createDocumentFragment();
 document.getElementById('btnAgregar').addEventListener('click',function(e){
   var autor = document.getElementById('textAutor').value;
   var contentPost = document.getElementById('contentPost').value;
-  var titlePost = document.createElement('h3');
-  titlePost.textContent = autor;
+  var titlePost = document.createElement('h6');
+  titlePost.textContent = "Autor : "+autor;
   var newPost = document.createElement('p');
   newPost.setAttribute('class','post classMargin');
   newPost.textContent = contentPost;
   newPost.prepend(titlePost);
-  dfm.appendChild(newPost);
-  document.getElementById('post').prepend(dfm);
+  if(contentPost !== ''){
+    dfm.appendChild(newPost);
+    document.getElementById('post').prepend(dfm);
+    document.getElementById('textAutor').value = "";
+    document.getElementById('contentPost').value = "";
+  }else {
+    alert('Ingrese Contenido');
+  }
 });
